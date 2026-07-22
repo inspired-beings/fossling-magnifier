523fdad5aa002c3899061dfdad6cfd88c3cb4fae
Ivan Gabriele <ivan.gabriele@protonmail.com>
2026-07-22 10:14:26 +0200

    test: add localized pump helper
    Signed-off-by: Ivan Gabriele <ivan.gabriele@protonmail.com>

---
+++ test/helpers/pump_localized.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:floss_magnifier/l10n/generated/app_localizations.dart';
+
+Future<void> pumpLocalized(
+  WidgetTester tester,
+  Widget child, {
+  Locale locale = const Locale('en'),
+}) {
+  return tester.pumpWidget(MaterialApp(
+    locale: locale,
+    localizationsDelegates: AppLocalizations.localizationsDelegates,
+    supportedLocales: AppLocalizations.supportedLocales,
+    home: Scaffold(body: child),
+  ));
+}
